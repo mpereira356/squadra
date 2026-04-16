830f9692cb750326218ceff27f642682038c226a
mpereira356 <viny_gtb@hotmail.com>
2026-04-16 14:29:39 -0300

    att

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return send_from_directory(app.root_path, 'index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
